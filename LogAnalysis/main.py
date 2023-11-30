d3b058035a48446d8eafd50729dcab8bedf969c4
Abdulkerim Yenidoğan <kerimyenidogan01@gmail.com>
2023-11-30 09:54:17 +0300

    vizerapor
    update

--- LogAnalysis/main.py
+++ LogAnalysis/main.py
@@ -7,6 +7,11 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg
 veriSayac=0
 tarih=[]
 adet=[]
+uyari=[]
+adet2=[]
+bugununLogSayisi=""
+dununLogSayisi=""
+gunsayisi=0
 def powerShellFonksiyon():
     # PowerShellden istediğim log düzeni ile loglarımı python projemin bulunduğu dizinde çalıştırılmasını sağladım.
     cmdKomut = "Get-WinEvent -LogName 'System' | Select-Object -Property TimeCreated, Id, LevelDisplayName, Message | Format-Table -AutoSize | Out-File -FilePath .\\Log.txt"
@@ -30,10 +35,11 @@ def tarihAyristirma():
                     tarih_sozlugu[tarih] = 1
                     veriSayac +=1
 
-
     with open("TarihAyristirlmis.txt", "w", encoding="utf-16") as dosya2:  # Tarih adlı dosyama ayrıştırdığım verileri yazıyorum
         for tarih, sayi in tarih_sozlugu.items():
             dosya2.write(tarih + " " + str(sayi) + "\n")
+            global gunsayisi
+            gunsayisi=gunsayisi+1
 
     # Sonuçları ekrana yazdırın
     for tarih, sayi in tarih_sozlugu.items():
@@ -41,19 +47,18 @@ def tarihAyristirma():
 
     # Dosyayı kapatın
     dosya.close()
-
-
 def logAyristirma():
     logDeseni = r"(\d{2}/\d{2}/\d{4}\s+\d{2}:\d{2}:\d{2})\s+(\d+)\s+(\w+)\s+(.+)"
     #regexin arayacağı log düzeni
     with open("Log.txt", "r", encoding="utf-16") as dosya:
         uyariTip = {}  # UYARI TİPLERİNİ HAFIZADA TUTACAK SÖZLÜK
+
         uyari=""
         for satir in dosya:
             match = re.match(logDeseni, satir)
 
             if match:
-                kod, seviye = match.group(2), match.group(3)
+                tarih,kod, seviye =match.group(0), match.group(2), match.group(3)
                 uyari=kod+" "+seviye
             if uyari in uyariTip:
                 uyariTip[uyari] += 1
@@ -63,7 +68,6 @@ def logAyristirma():
             for uyari, sayi in uyariTip.items():
                 dosya2.write(f'{uyari} {sayi} \n')
                 print(f"{uyari}: {sayi} adet")
-
 def cizgiGrafik():
     # Matplotlib grafiği oluşturun
     fig = Figure(figsize=(6, 4), dpi=100)
@@ -79,12 +83,11 @@ def cizgiGrafik():
 
 
 
-
-
 powerShellFonksiyon()
 tarihAyristirma()
 logAyristirma()
 
+#Ayrıştırılmış tarihleri okuyarak listeme ekliyorum ve cizgigrafik fonksiyonuma yolluyorum...
 with open("TarihAyristirlmis.txt", "r", encoding="utf-16") as dosya:
     for satir in dosya:
         satir = satir.strip().split()  # Satırı boşluklara göre bölelim
@@ -93,14 +96,27 @@ with open("TarihAyristirlmis.txt", "r", encoding="utf-16") as dosya:
 
 cizgiGrafik()
 
+#Ayrıştırılmış uyari tiplerini listeme ekliyorum ve flask ile web siteme yönlendiriyorum...
+with open("UyariAyristirlmis.txt", "r", encoding="utf-16") as dosya2:
+    for indeks, satir in enumerate(dosya2):
+        if indeks==0:
+            continue
+        else:
+            satir = satir.strip().split()  # Satırı boşluklara göre bölelim
+            uyari.append(satir[0]+" "+satir[1])
+            adet2.append(int(satir[2]))  # İkinci kelime adet sayısı
 
+bugununLogSayisi=str(adet[0])
+dununLogSayisi=str(adet[1])
+ortalamahesaplama=veriSayac/gunsayisi
+print(f"Ortlama Günlük Log sayisi {ortalamahesaplama}")
+
+
+#FLASK KISMI
 app = Flask(__name__)
-
 @app.route('/')
 def AnaSayfa():
-    return render_template('grafik.html',veriSayac=veriSayac)
-
-
+    return render_template('anasayfa.html',veriSayac=veriSayac,uyari=uyari,adet2=adet2,bugununLogSayisi=bugununLogSayisi,dununLogSayisi=dununLogSayisi,gunsayisi=gunsayisi)
 if __name__ == '__main__':
     app.run(debug=True)
 
